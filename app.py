fb48ddd3afe5838e1003f516583a0b7744be3931
NonyaneTree <thulanenonyane1@gmail.com>
2025-07-25 17:43:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ CHAT_ID = os.environ.get("CHAT_ID")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("register.html")
 
 @app.route("/register", methods=["POST"])
 def register():
