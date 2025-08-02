0337ab5be0c9b8d79e8bdc44c41025c6bfc76d89
NonyaneTree <nonyaneinvestmenttrees1@gmail.com>
2025-08-02 19:03:13 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:your_password@localhost:5432/realtraders")
+EMAIL = "nonyaneinvestmenttree@gmail.com"
+APP_PASSWORD = "qiku gpty uxso ygwi"  # Google App Password
