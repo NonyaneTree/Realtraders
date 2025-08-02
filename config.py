40ef5e713d4783b831ac573fc940916d2f443a63
NonyaneTree <nonyaneinvestmenttrees1@gmail.com>
2025-08-02 19:45:14 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-import os
-
-DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:your_password@localhost:5432/realtraders")
+DATABASE_URI = "postgresql://real_traders_user:9hqUS4p6Vx7T4gam40WJ0y8hIUmct9al@dpg-d274k83uibrs73cup0j0-a.oregon-postgres.render.com/real_traders"
 EMAIL = "nonyaneinvestmenttree@gmail.com"
-APP_PASSWORD = "qiku gpty uxso ygwi"  # Google App Password
+APP_PASSWORD = "qiku gpty uxso ygwi"
