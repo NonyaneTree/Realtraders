fcbd14f13f1b61e9d18ea51d546405066cbef88d
NonyaneTree <nonyaneinvestmenttrees1@gmail.com>
2025-08-02 19:05:23 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    experience = db.Column(db.String(50))
+    secret = db.Column(db.String(120))
+    password = db.Column(db.String(120))
