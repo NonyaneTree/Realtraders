7af9e111da2e30c686372655aa200ee3254baa52
NonyaneTree <nonyaneinvestmenttrees1@gmail.com>
2025-08-02 22:23:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import smtplib
 import random
 
 # === CONFIGURATION ===
-DATABASE_URI = ""
+DATABASE_URI = "postgresql://real_traders_user:9hqUS4p6Vx7T4gam40WJ0y8hIUmct9al@dpg-d274k83uibrs73cup0j0-a/real_traders"
 EMAIL = "nonyaneinvestmenttree@gmail.com"  # 🔁 Replace with your Gmail
 APP_PASSWORD = "qiku gpty uxso ygwi"  # 🔁 Replace with your Gmail App Password
 
